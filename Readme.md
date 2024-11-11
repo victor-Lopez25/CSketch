bcf3d38dae6d719435620b1ecbe848d017827375
victor-Lopez25 <victorlopezcortes25@gmail.com>
2024-11-11 16:12:51 +0100

    Initial commit v2

--- Readme.md
+++ Readme.md
@@ -1,15 +1,18 @@
-## CSketch
+## CPixelSketch
 
-Sketch program made in C
+Sketch program made in C with SDL3
+
+WARNING: This is not complete yet!
 
 ## Dependencies
 
-- [SDL2](https://www.libsdl.org/)
+- [SDL3](https://github.com/libsdl-org/SDL/releases/tag/preview-3.1.6)
 
 ## Building
 
 If on windows, use build.bat after initializing cl.exe (MSVC) with vcvarsall.bat
+Must have SDL3.dll (at least version 3.1.6) in bin directory
 
 ## Licenses
 
-[CSketch]() © 2024 by [Víctor López Cortés](https://github.com/victor-Lopez25) is licensed under [CC BY 4.0](https://creativecommons.org/licenses/by/4.0/)
+[CPixelSketch]() © 2024 by [Víctor López Cortés](https://github.com/victor-Lopez25) is licensed under [CC BY 4.0](https://creativecommons.org/licenses/by/4.0/)
--- build.bat
+++ build.bat
@@ -1,16 +1,18 @@
 @echo off
 
-set out=csketch.exe
-set sdllibpath=C:\SDL2-2.26.1\lib\x64\
-set sdlincludepath=C:\SDL2-2.26.1\include\
-set defines=-D_CRT_SECURE_NO_WARNINGS
-set opts=-GR- -EHa- -nologo -Zi -W4
-set code=%cd%\src
+:: Find SDL3 here:
+:: https://github.com/libsdl-org/SDL/releases/tag/preview-3.1.6
+set sdllibpath=D:\SDL3-3.1.6\lib\x64\
+set sdlincludepath=D:\SDL3-3.1.6\include\
+set cpath=..\src
+set out=pixel_sketch.exe
+set opts=-FC -GR- -EHa- -W4 -nologo -Zi
+set csrc=%cpath%\main.c
 
-copy dependencies\sdl2.dll bin\sdl2.dll /Y > NUL
+if not exist bin mkdir bin
 
 pushd bin
-cl %defines% %opts% %code%\main.cpp -Fe%out% -I%sdlincludepath% /link /libpath:%sdllibpath% SDL2.lib -incremental:no
-::%out% drawing.bmp
+cl %opts% %csrc% -Fe%out% -I%sdlincludepath% /link /libpath:%sdllibpath% SDL3.lib -incremental:no
 
+::%out%
 popd
